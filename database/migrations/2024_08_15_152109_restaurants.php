19a0160596981b924c79de61b2bccd661cbaa38b
unknown <singhaldivyanshu2001@gmail.com>
2024-08-15 21:22:44 +0530

    intialized and added migrations

---
+++ database/migrations/2024_08_15_152109_restaurants.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('restaurants',function(Blueprint $table){
+            $table->id();
+            $table->integer('master_restaurant_brand_id');
+            $table->integer('master_location_id');
+            $table->float('rating');
+            $table->integer('deal_amount');
+            $table->string('deal_options');
+            $table->integer('bought_count');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
